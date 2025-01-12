c758171a092fd82f9617d027bd13d704bece5621
victor06-git <asensiovector@gmail.com>
2025-01-12 12:54:33 +0100

    fixed: issue with import org.json.JSONArray and org.json.JSONObject

--- gestor_biblioteca/gestor/src/main/java/gestor/biblioteca/Main.java
+++ gestor_biblioteca/gestor/src/main/java/gestor/biblioteca/Main.java
@@ -8,9 +8,6 @@ public class Main {
         System.out.println("Hello world!");
         JSONArray jsonArray = new JSONArray();
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("name", "John");
-        jsonObject.put("age", 30);
-        jsonArray.put(jsonObject);
-        System.out.println(jsonArray.toString());
+        
     }
 }
