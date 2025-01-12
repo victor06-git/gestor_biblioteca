47def5d4462783a4b12cfe6517765e79eb64fd42
victor06-git <asensiovector@gmail.com>
2025-01-12 12:42:29 +0100

    pom.xml actualització

--- gestor_biblioteca/gestor/src/main/java/gestor/biblioteca/Main.java
+++ gestor_biblioteca/gestor/src/main/java/gestor/biblioteca/Main.java
@@ -1,7 +1,16 @@
 package gestor.biblioteca;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        JSONArray jsonArray = new JSONArray();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", "John");
+        jsonObject.put("age", 30);
+        jsonArray.put(jsonObject);
+        System.out.println(jsonArray.toString());
     }
 }
