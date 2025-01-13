1d11a7765e16364e917e619ae793b057f2d6550b
victor06-git <asensiovector@gmail.com>
2025-01-13 15:34:21 +0100

    fix: Actualització millora funció

--- gestor_biblioteca/gestor/src/main/java/gestor/biblioteca/Main.java
+++ gestor_biblioteca/gestor/src/main/java/gestor/biblioteca/Main.java
@@ -1,11 +1,10 @@
 package gestor.biblioteca;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Main {
     
@@ -13,6 +12,12 @@ public class Main {
     public static String llibresNomLlistat(String title) {
         // Function to get the list of books from the JSON file by name
         String filePath_books = "./data/llibres.json";
+
+        Integer nomLlibreWidth = 10;
+        Integer autorLlibreWidth = 15;
+
+        StringBuilder llibresNomLlistat = new StringBuilder(String.format("%-" + nomLlibreWidth + "s %-" +
+                    autorLlibreWidth + "s%n", "Nom", "Autor"));
         
         try {
             String content = new String(Files.readAllBytes(Paths.get(filePath_books)));
