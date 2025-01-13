d9d32d07e8159e7f0e921667de4b49f550316971
victor06-git <asensiovector@gmail.com>
2025-01-13 18:30:19 +0100

    acualització branca

--- gestor_biblioteca/gestor/src/main/java/gestor/biblioteca/Main.java
+++ gestor_biblioteca/gestor/src/main/java/gestor/biblioteca/Main.java
@@ -17,7 +17,16 @@ public class Main {
     public static String llibresNomLlistat(String title) {
         // Function to get the list of books from the JSON file by name
         String filePath_books = "./data/llibres.json";
-        
+
+        StringBuilder llibresNom = new StringBuilder();
+
+        Integer nomLlibreWidth = 25;
+        Integer autorLlibreWidth = 15;
+        //Integer space = 5;
+
+        llibresNom.append(String.format("%-" + nomLlibreWidth + "s %-" + autorLlibreWidth + "s%n", "Titol", "Autor"));
+        llibresNom.append("--------                  ----------\n");
+
         try {
             String content = new String(Files.readAllBytes(Paths.get(filePath_books)));
             
@@ -26,11 +35,14 @@ public class Main {
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                 String bookTitle = jsonObject.getString("titol");
+                String autorNom = jsonObject.getString("autor");
 
+                
                 if (bookTitle.toLowerCase().contains(title.toLowerCase())) {
-                    return jsonObject.toString();
+                    llibresNom.append(String.format("%-" + nomLlibreWidth + "s %-" + autorLlibreWidth + "s%n", bookTitle, autorNom));
                 }
             }
+            return llibresNom.toString();
 
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
@@ -85,40 +97,115 @@ public class Main {
     public static String prestecsLlista() {
         // Function to get the list of books from the JSON file
         String filePath_prestecs = "./data/prestecs.json";
+        String filePath_llibres = "./data/llibres.json";
+        String filePath_usuaris = "./data/usuaris.json";
         StringBuilder prestecList = new StringBuilder();
 
-        int idLlibreWidth = 10;
-        int idUsuariWidth = 10;
+        int idWidth = 5;
+        int idLlibreWidth = 35;
+        int idUsuariWidth = 25;
         int dataPrestecWidth = 15;
         int dataDevolucioWidth = 15;
 
-        prestecList.append(String.format("%-" + idLlibreWidth + "s %-" + 
+        prestecList.append(String.format("%-" + idWidth + "s %-" + idLlibreWidth + "s %-" + 
                         idUsuariWidth + "s %-" + dataPrestecWidth + "s %-" +
-                        dataDevolucioWidth + "s%n", "IdLlibre", "IdUsuari", "Data Prestec", "Data Devolucio"));
-        prestecList.append("----------------------------------------------------\n");
+                        dataDevolucioWidth + "s%n", "ID","Llibre", "Usuari", "Data Prestec", "Data Devolucio"));
+        prestecList.append("--------------------------------------------------------------------------------------------\n");
 
         try {
             String content = new String(Files.readAllBytes(Paths.get(filePath_prestecs)));
+            String content_2 = new String(Files.readAllBytes(Paths.get(filePath_llibres)));
+            String content_3 = new String(Files.readAllBytes(Paths.get(filePath_usuaris)));
 
             JSONArray jsonArray = new JSONArray(content);
+            JSONArray jsonArray2 = new JSONArray(content_2);
+            JSONArray jsonArray3 = new JSONArray(content_3);
+
+            String idNom = "";
+            String idLlibreNom = "";
 
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                 Integer idLlibre = jsonObject.getInt("IdLlibre");
                 Integer idUsuari = jsonObject.getInt("IdUsuari");
+                
+                for (int j = 0; j < jsonArray2.length(); j++) {
+                    JSONObject jsonObject2 = jsonArray2.getJSONObject(j);
+                    Integer idLlibre2 = jsonObject2.getInt("id");
+                    String titolLlibre = jsonObject2.getString("titol");
+
+                    if (idLlibre == idLlibre2) {
+                        idLlibreNom = titolLlibre;
+                    }
+                }
+
+                for (int k = 0; k < jsonArray3.length(); k++) {
+                    JSONObject jsonObject3 = jsonArray3.getJSONObject(k);
+                    Integer idUsuari2 = jsonObject3.getInt("id");
+                    String nomUsuari = jsonObject3.getString("nom");
+                    String cognomUsuari = jsonObject3.getString("cognoms");
+
+                    if (idUsuari == idUsuari2) {
+                        idNom = nomUsuari + " " + cognomUsuari;
+                    }
+                }
+
                 String dataPrestec = jsonObject.getString("DataPrestec");
                 String dataDevolucio = jsonObject.getString("DataDevolucio");
 
-                prestecList.append(String.format("%-" + idLlibreWidth + "s %-" + idUsuariWidth + "s %-" +
-                                    dataPrestecWidth + "s %-" + dataDevolucioWidth + "s%n", idLlibre, idUsuari, dataPrestec, dataDevolucio));
-            }
+                prestecList.append(String.format("%-" + idWidth + "s %-" +  idLlibreWidth + "s %-" + idUsuariWidth + "s %-" +
+                                    dataPrestecWidth + "s %-" + dataDevolucioWidth + "s%n", idLlibre, idLlibreNom, idNom, dataPrestec, dataDevolucio));
+            }       
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
             }
         return prestecList.toString();
     }
 
+    /**
+     * Funció que llistar tots els llibres (.json) de la biblioteca.
+     * 
+     * @return Retorna un array dels llibres segons id, titol i autor.
+     */
+    public static String llibresLlistat(){
+        // Ruta arxiu "llibres.json"
+        String filePath_llibres = "./data/llibres.json";
+        StringBuilder llibresList = new StringBuilder();
+
+        int idWidth = 5;
+        int titolWidth = 35;
+        int autorWidth = 25;
+
+        // Crear les columnes
+        llibresList.append(String.format("%-" + idWidth + "s %-"
+                        + titolWidth + "s %-" + autorWidth + "s%n", "ID", "Títol", "Autor"));
+                        llibresList.append("---------------------------------------------------------------------\n");
+
+        //Iterar sobre cada llibre
+        try{
+            String content = new String(Files.readAllBytes(Paths.get(filePath_llibres)));
+            JSONArray jsonArray = new JSONArray(content);
+
+            for(int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                int id = jsonObject.getInt("id");
+                String titol = jsonObject.getString("titol");
+                String autor = jsonObject.getString("autor");
+
+                llibresList.append(String.format("%-" + idWidth + "s %-"
+                + titolWidth + "s %-" + autorWidth + "s%n", id, titol, autor));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            }
+        return llibresList.toString();
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        
+        System.out.println(prestecsLlista());
+        
     }
 }
