c67651d97f7059dfb1d937da23cfbefd3af8c029
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-01-17 17:55:42 +0100

    feat: función eliminar libro
    Se eliminan los libros según su título y autor

--- gestor_biblioteca/gestor/src/main/java/gestor/biblioteca/Main.java
+++ gestor_biblioteca/gestor/src/main/java/gestor/biblioteca/Main.java
@@ -442,6 +442,59 @@ public class Main {
         }
     }
 
+    public static void eliminarLLibre(){
+        String filePathLlibres = "./data/llibres.json";
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String contenido = new String(Files.readAllBytes(Paths.get(filePathLlibres)));
+            JSONArray jsonArray = new JSONArray(contenido);
+
+            System.out.print("Introduce el titulo del libro a eliminar: ");
+            String titol = scanner.nextLine().trim();
+
+            System.out.print("Introduce el autor del libro: ");
+            String autor = scanner.nextLine().trim();
+
+            boolean found = false;
+
+            /* Buscar libro pot titulo y autor */
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject librosExistentes = jsonArray.getJSONObject(i);
+                String tituloExistente = librosExistentes.getString("titol");
+                String autorExistente = librosExistentes.getString("autor");
+
+                if (tituloExistente.equals(titol) && autorExistente.equals(autor)){
+                    System.out.print("¿Estas seguro que quieres eliminar el libro " + titol + "  de" + autor + "? (s/n): ");
+                    String confirmar = scanner.nextLine().trim().toLowerCase();
+
+                    if (confirmar.equals("s")){
+                        jsonArray.remove(i);
+                        found = true;
+
+                        try (FileWriter file = new FileWriter(filePathLlibres)) {
+                            file.write(jsonArray.toString(4));
+                        }
+                        System.out.println("Libro eliminado co éxito.");
+
+                    } else {
+                        System.out.println("Operación cancelada");
+                    }
+                    break;
+                }
+            }
+            
+            if (!found) {
+                System.out.println("No se encontró un libro con los datos proporcionados.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer o escribir el archivo: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error inesperado: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -457,8 +510,9 @@ public class Main {
         /*System.out.println(foraterminilist);
        /*  System.out.println(afegirusuario);*/
         /*System.out.println(autorlistado);*/
-        afegirUsuari();
+        /*afegirUsuari();*/
         /*eliminarUsuario();*/
+        eliminarLLibre();
 
     }
 }
