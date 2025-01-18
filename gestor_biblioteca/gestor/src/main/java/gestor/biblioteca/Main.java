6f5dc7bb30d490308cbc98fd75fa2e87e805596d
samantham1812 <150824201+samantham1812@users.noreply.github.com>
2025-01-18 22:13:06 +0100

    feat: función eliminar libro

--- gestor_biblioteca/gestor/src/main/java/gestor/biblioteca/Main.java
+++ gestor_biblioteca/gestor/src/main/java/gestor/biblioteca/Main.java
@@ -371,6 +371,61 @@ public static void afegir_llibres() {
         }
     }
 
+    public static void eliminarLLibre(){
+        String filePathLlibres = "./data/llibres.json";
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            String contenido = new String(Files.readAllBytes(Paths.get(filePathLlibres)));
+            JSONArray jsonArray = new JSONArray(contenido);
+
+            System.out.print("Introduce el titulo del libro a eliminar: ");
+            String titol = scanner.nextLine().trim();
+
+            System.out.print("Introduce el autor del libro: ");
+            String autor = scanner.nextLine().trim();
+
+            boolean found = false;
+            int indexToRemove = -1;
+
+            /* Buscar libro pot titulo y autor */
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject librosExistentes = jsonArray.getJSONObject(i);
+                String tituloExistente = librosExistentes.getString("titol").trim();
+                String autorExistente = librosExistentes.getString("autor").trim();
+
+                if (tituloExistente.equals(titol) && autorExistente.equals(autor)){
+                    System.out.print("¿Estas seguro que quieres eliminar el libro " + titol + " de " + autor + "? (s/n): ");
+                    String confirmar = scanner.nextLine().trim().toLowerCase();
+
+                    if (confirmar.equals("s")){
+                        jsonArray.remove(i);
+                        found = true;
+
+                        try (FileWriter file = new FileWriter(filePathLlibres)) {
+                            file.write(jsonArray.toString(4));
+                        }
+                        System.out.println("Libro eliminado con éxito.");
+
+                    } else {
+                        System.out.println("Operación cancelada");
+                    }
+                    break;
+                }
+            }
+            
+            if (!found) {
+                System.out.println("No se encontró un libro con los datos proporcionados.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer o escribir el archivo: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error inesperado: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+
     public static void eliminar_prestecs() {
         Scanner scanner = new Scanner(System.in);
         String filePathPrestecs = "./data/prestecs.json";
